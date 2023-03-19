670957e352d75a2a7c4f0bca2fccb6c8f4bbd4cf
Koray <91742967+Koraytopuz@users.noreply.github.com>
2023-03-19 20:50:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+      byte vByte=10;
+      short vShort=100;
+      int vInt=1000;
+      long vLong=100000;
+      System.out.println(vByte);
+      System.out.println(vShort);
+      System.out.println(vInt);
+      System.out.println(vLong);
+    }
+}
